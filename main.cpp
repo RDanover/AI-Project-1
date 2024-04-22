1f61b2b1909c4646240be5bb7cf094a2889c2c6d
Rachel Danover <racheldanover@ucr-secure-35-10-13-191-30.wnet.ucr.edu>
2024-04-22 12:51:37 -0700

    implemented solve_puzzle function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+//TBD: 
+//      - implement get_possible_moves
+//      - allow user to enter puzzle
+//      - implement heuristics
 
-void print_puzzle(int p[], int s, int r){
+
+#include <iostream>
+#include <vector>
+
+const int puzzle_size = 9;
+const int row_size = 3;
+const std::vector<int> goal_state = {1,2,3,4,5,6,7,8,0};
+std::vector<int> puzzle = {1,2,3,4,5,6,7,0,8};
+
+std::vector<std::vector<int>> get_possible_moves (std::vector<int> p){
+    std::vector<std::vector<int>> possible_moves;
+    //get possible moves 
+    return possible_moves;
+}
+
+void solve_puzzle(std::vector<int> p ){
+    std::vector<std::vector<int>> queue;
+    queue.push_back(p);
+    std::vector<int> current_node;
+    while (queue.size() != 0){
+        current_node = queue.front();
+        queue.erase(queue.begin());
+        print_puzzle(current_node, puzzle_size, row_size);
+        if(current_node == goal_state){
+            std::cout<<("Solution found!\n");
+            return;
+        }
+        std::cout<<("Expanding State: \n");
+        std::vector<std::vector<int>> possible_moves = get_possible_moves(current_node);
+        for(int i = 0; i<possible_moves.size();i++){
+            queue.push_back(possible_moves[i]);
+        }
+    }
+    std::cout<<("This puzzle has no solution.\n");
+}
+
+void print_puzzle(std::vector<int> p, int s, int r){
     for(int i = 1; i<=s;i++){
         std::cout<<(p[i-1]);
         if(i%r==0){
@@ -13,9 +52,6 @@ void print_puzzle(int p[], int s, int r){
 }
 
 int main (){
-    const int puzzle_size = 9;
-    int row_size = 3;
-    int puzzle[puzzle_size] = {1,2,3,4,5,6,7,0,8};
     char input = ' ';
     std::cout<<("Would you like to enter a puzzle to be solved? Y or N?\n");
     std::cin>>(input);
